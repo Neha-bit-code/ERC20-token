a01971dd1d2215fefb38b1bfb9a64d0961ae701a
NEHA VERMA <154819639+Neha-bit-code@users.noreply.github.com>
2024-06-26 21:06:31 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -7,4 +7,16 @@ contract MyERC20Token is ERC20 {
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
     }
+
+    // Explicit burn function
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Override transfer function
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
 }
+ 
